146a42ff6bc14d8e225c636a80915a125d906006
Vadym Kushnirenko <48694851+kushnirenko-remme@users.noreply.github.com>
2019-12-30 18:44:55 +0200

    Update remprotocol.rb

--- remprotocol.rb
+++ remprotocol.rb
@@ -2,8 +2,8 @@ class Remprotocol < Formula
 
    homepage "https://github.com/Remmeauth/remprotocol"
    revision 0
-   url "https://github.com/RemmeAuth/remprotocol/archive/0.3.0.tar.gz"
-   version "0.3.0"
+   url "https://github.com/RemmeAuth/remprotocol/archive/0.3.1.tar.gz"
+   version "0.3.1"
 
    option :universal
 
@@ -15,8 +15,8 @@ class Remprotocol < Formula
    depends_on :arch =>  :intel
 
    bottle do
-      root_url "https://github.com/RemmeAuth/remprotocol/releases/download/0.3.0"
-      sha256 "a3f8f19fe078726f065db0b278de7936c6ef6305a6cb3f85167efeb783bdd78a" => :mojave
+      root_url "https://github.com/RemmeAuth/remprotocol/releases/download/0.3.1"
+      sha256 "1c84c1a332d493f7e6202b0cb65bc87cef25c1af131a2a7fdd258afd5bfbcc19" => :mojave
    end
    def install
       raise "Error, only supporting binary packages at this time"
